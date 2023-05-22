ca243ea972a31049ba56122389f10d63411bac2e
Hardik Sharma <98525131+hardiknsharma@users.noreply.github.com>
2023-05-22 20:41:03 +0530

    Add files via upload

---
+++ Index.aspx.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Imports System.Configuration
+
+
+Partial Class Index
+    Inherits System.Web.UI.Page
+    Dim DM As New WEDDINGBELLSDataContext
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnSubmit.Click
+
+        Dim WED As New ADM_CONTACT_US
+        WED.YOUR_NAME = txtYourName.Text
+        WED.EMAIL_ID = txtYourEmail.Text
+        WED.MOBILE_NO = txtMobileNo.Text
+        WED.SUBJECT = txtSubject.Text
+        WED.MESSAGE = txtMessage.InnerText
+        DM.ADM_CONTACT_US.InsertOnSubmit(WED)
+        DM.SubmitChanges()
+
+        Response.Redirect("Index.aspx")
+
+
+    End Sub
+End Class
